680950b0e1030814f928f5789846b823c851a72c
Painter <paint163@gmail.com>
2019-03-16 06:03:47 +0400

    [-] event listener click on icon-close

--- js.js
+++ js.js
@@ -2,7 +2,4 @@ jQuery(document).ready(function () {
     jQuery('.bubble-block').on('click', '.bubble-ico', function () {
         jQuery('#card').fadeToggle();
     })
-    jQuery('#card').on('click', '.button-close', function () {
-        jQuery('#card').fadeOut();
-    })
 })
